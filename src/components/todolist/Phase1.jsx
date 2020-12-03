262a19f4f5b0eba3b186433c09ef421637b4edcb
Sulistyo Ardani <sulistyoardani@gmail.com>
2020-12-04 01:49:55 +0700

    todolist

--- src/components/todolist/Phase1.jsx
+++ src/components/todolist/Phase1.jsx
@@ -36,7 +36,7 @@ const Phase1 = () => {
       <div className="h-100 w-full flex items-center justify-center bg-green-900-lightest font-sans">
         <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
           <div className="mb-4">
-            <h1 className="text-grey-darkest">Todo List</h1>
+            <h1 className="">Todo List</h1>
             <div className="flex mt-4">
               <input
                 name="todoText"
