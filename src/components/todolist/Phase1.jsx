b32db78f4389ac694af98debc66423dec1022add
Sulistyo Ardani <sulistyoardani@gmail.com>
2020-12-04 01:33:28 +0700

    todolist

--- src/components/todolist/Phase1.jsx
+++ src/components/todolist/Phase1.jsx
@@ -33,7 +33,7 @@ const Phase1 = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+      <div className="h-100 w-full flex items-center justify-center bg-green-900-lightest font-sans">
         <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
           <div className="mb-4">
             <h1 className="text-grey-darkest">Todo List</h1>
@@ -51,7 +51,7 @@ const Phase1 = () => {
               <button
                 onClick={() => submit()}
                 name="button"
-                className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal"
+                className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-green-900"
               >
                 Add
               </button>
@@ -62,7 +62,7 @@ const Phase1 = () => {
             {Object.keys(arr).map((keyName, i) => (
               <div id={keyName} className="flex mb-4 items-center">
                 {arr[keyName]["status"] ? (
-                  <p className="w-full line-through text-green ">
+                  <p className="w-full line-through text-green-400 ">
                     {arr[keyName]["name"]}
                   </p>
                 ) : (
@@ -71,13 +71,13 @@ const Phase1 = () => {
 
                 <button
                   onClick={() => status(i)}
-                  className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green"
+                  className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-400 border-green hover:bg-green-400"
                 >
                   {arr[keyName]["status"] ? "Done" : "unDone"}
                 </button>
                 <button
                   onClick={() => removeKey(i)}
-                  className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red"
+                  className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-400 border-red hover:text-white hover:bg-red-400"
                 >
                   Remove
                 </button>
@@ -85,13 +85,13 @@ const Phase1 = () => {
             ))}
 
             {/* <div className="flex mb-4 items-center">
-              <p className="w-full line-through text-green">
+              <p className="w-full line-through text-green-400">
                 Submit Todo App Component to Tailwind Components
               </p>
               <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey">
                 Not Done
               </button>
-              <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">
+              <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-400 border-red hover:text-white hover:bg-red-400">
                 Remove
               </button>
             </div> */}
