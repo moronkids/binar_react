6a24ba43316a3ed0d90dbe66da90d8aa011b8261
Sulistyo Ardani <sulistyoardani@gmail.com>
2020-12-04 01:00:49 +0700

    todolist

--- src/components/todolist/Phase1.jsx
+++ src/components/todolist/Phase1.jsx
@@ -73,7 +73,7 @@ const Phase1 = () => {
                   onClick={() => status(i)}
                   className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green"
                 >
-                  Done
+                  {arr[keyName]["status"] ? "Done" : "unDone"}
                 </button>
                 <button
                   onClick={() => removeKey(i)}
